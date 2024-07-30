8f2d120dad68b1eb3c0661734fe372e494c52ba2
m. andres novrizal <m.andres.novrizal@gmail.com>
2024-07-30 17:37:57 +0700

    fixed

--- helper/setupGoogleAuth.go
+++ helper/setupGoogleAuth.go
@@ -12,8 +12,9 @@ func SetupGoogleAuth() *oauth2.Config {
 		ClientID:     os.Getenv("CLIENT_ID"),
 		ClientSecret: os.Getenv("CLIENT_SECRET"),
 		RedirectURL:  "https://manajementugas.com/auth/callback",
-		Scopes:       []string{"email", "profile"},
-		Endpoint:     google.Endpoint,
+		//RedirectURL:  "http://127.0.0.1:4040/auth/callback",
+		Scopes:   []string{"email", "profile"},
+		Endpoint: google.Endpoint,
 	}
 	return config
 }
