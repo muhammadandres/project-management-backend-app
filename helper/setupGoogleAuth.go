127858e3e07d69a5c9f7e6113d9b3a355733f80d
m. andres novrizal <m.andres.novrizal@gmail.com>
2024-07-31 16:35:13 +0700

    fixed

--- helper/setupGoogleAuth.go
+++ helper/setupGoogleAuth.go
@@ -3,20 +3,26 @@ package helper
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"fmt"
+	"os"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
-func SetupGoogleAuth() *oauth2.Config {
-	config := &oauth2.Config{
-		ClientID:     "91088718933-p44u1h8q4n5s5hrbj6rkdbhk58tgfhce.apps.googleusercontent.com",
-		ClientSecret: "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",
-		RedirectURL:  "https://www.manajementugas.com/auth/callback",
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
-		Endpoint:     google.Endpoint,
+func SetupGoogleAuth() (*oauth2.Config, error) {
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		return nil, fmt.Errorf("unable to read client secret file: %v", err)
 	}
-	return config
+
+	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile")
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse client secret file to config: %v", err)
+	}
+
+	config.RedirectURL = "https://www.manajementugas.com/auth/callback"
+	return config, nil
 }
 
 func GenerateRandomState() string {
