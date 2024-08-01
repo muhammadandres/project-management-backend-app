88ecba12175ed94a42f0aea9301b98903eb3990f
m. andres novrizal <m.andres.novrizal@gmail.com>
2024-08-01 23:54:45 +0700

    fixed

--- helper/envVars.go
+++ helper/envVars.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -11,5 +12,12 @@ func LoadEnv() {
 		log.Println("No .env file found or error loading it. Using environment variables directly.")
 	} else {
 		log.Println("Successfully loaded .env file")
+	}
+
+	requiredEnvVars := []string{"PORT", "DB_URL", "SECRET", "AWS_REGION", "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "DB_HOST", "DB_USER", "DB_PASSWORD", "DB_NAME", "GOOGLE_CALENDAR_CREDENTIALS"}
+	for _, envVar := range requiredEnvVars {
+		if os.Getenv(envVar) == "" {
+			log.Fatalf("Required environment variable %s is not set", envVar)
+		}
 	}
 }
