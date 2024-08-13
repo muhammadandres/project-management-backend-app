48fbf1f6701a20e03e4609dfb1e9d588f7bfe59d
m. andres novrizal <m.andres.novrizal@gmail.com>
2024-08-14 03:43:32 +0700

    fixed deployment

--- app/database.go
+++ app/database.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"manajemen_tugas_master/model/domain"
-	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -12,7 +11,7 @@ import (
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := os.Getenv("DB_URL")
+	dsn := "admin:andres12@tcp(manajemen-tugas-master.c5auaowcutch.ap-southeast-3.rds.amazonaws.com:3306)/manajementugasdb?charset=utf8mb4"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
