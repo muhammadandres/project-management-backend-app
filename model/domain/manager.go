150c24c9e827722fb15445faba4e2fae4915b0e7
m. andres novrizal <m.andres.novrizal@gmail.com>
2024-08-14 03:52:42 +0700

    fixed deployment

--- model/domain/manager.go
+++ model/domain/manager.go
@@ -1,7 +1,5 @@
 package domain
 
-import "time"
-
 type Manager struct {
 	ID               uint64     `json:"id" gorm:"primaryKey"`
 	Email            string     `json:"email" gorm:"size:255" validate:"email"`
@@ -10,7 +8,4 @@ type Manager struct {
 	InvitationID     *uint64    `json:"invitation_id,omitempty"`
 	InvitationStatus string     `json:"invitation_status,omitempty"`
 	Invitation       Invitation `json:"-" gorm:"foreignKey:InvitationID;references:ID"`
-	CreatedAt        time.Time  `json:"-"`
-	UpdatedAt        time.Time  `json:"-"`
-	DeletedAt        *time.Time  `json:"-"`
 }
--- model/domain/user.go
+++ model/domain/user.go
@@ -1,12 +1,7 @@
 package domain
 
-import "time"
-
 type User struct {
-	ID        uint64    `json:"id" gorm:"primaryKey"`
-	Email     string    `json:"email" gorm:"size:255" validate:"email"`
-	Password  string    `json:"password" gorm:"size:255"`
-	CreatedAt time.Time `json:"-"`
-	UpdatedAt time.Time `json:"-"`
-	DeletedAt *time.Time `json:"-"`
+	ID       uint64 `json:"id" gorm:"primaryKey"`
+	Email    string `json:"email" gorm:"size:255" validate:"email"`
+	Password string `json:"password" gorm:"size:255"`
 }
