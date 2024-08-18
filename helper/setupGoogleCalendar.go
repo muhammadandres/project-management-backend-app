e257a91d84fa5d32bb35cbdb3cc3a57e57786c4a
m. andres novrizal <m.andres.novrizal@gmail.com>
2024-08-18 15:56:38 +0700

    fixed

--- helper/setupGoogleCalendar.go
+++ helper/setupGoogleCalendar.go
@@ -7,8 +7,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
-	"runtime"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -16,28 +14,28 @@ import (
 	"google.golang.org/api/option"
 )
 
-func createCalendarService(userEmail string) (*calendar.Service, error) {
+func createCalendarService(senderEmail string) (*calendar.Service, error) {
 	credentials := os.Getenv("GOOGLE_CALENDAR_CREDENTIALS")
 
 	var credentialsJSON map[string]interface{}
 
-	// Coba parse sebagai JSON object terlebih dahulu
+	// Try to parse as JSON object first
 	err := json.Unmarshal([]byte(credentials), &credentialsJSON)
 	if err != nil {
-		// Jika gagal, coba parse sebagai string JSON
+		// If failed, try to parse as JSON string
 		var credentialsString string
 		err = json.Unmarshal([]byte(credentials), &credentialsString)
 		if err != nil {
 			return nil, fmt.Errorf("unable to parse GOOGLE_CALENDAR_CREDENTIALS: %v", err)
 		}
-		// Parse string JSON menjadi object
+		// Parse JSON string to object
 		err = json.Unmarshal([]byte(credentialsString), &credentialsJSON)
 		if err != nil {
 			return nil, fmt.Errorf("unable to parse GOOGLE_CALENDAR_CREDENTIALS content: %v", err)
 		}
 	}
 
-	// Konversi kembali ke JSON bytes untuk ConfigFromJSON
+	// Convert back to JSON bytes for ConfigFromJSON
 	credentialsBytes, err := json.Marshal(credentialsJSON)
 	if err != nil {
 		return nil, fmt.Errorf("unable to marshal credentials: %v", err)
@@ -49,7 +47,7 @@ func createCalendarService(userEmail string) (*calendar.Service, error) {
 	}
 
 	ctx := context.Background()
-	client := getClient(config, userEmail)
+	client := getClient(config, "m.andres.novrizal@gmail.com")
 
 	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
@@ -59,69 +57,16 @@ func createCalendarService(userEmail string) (*calendar.Service, error) {
 	return srv, nil
 }
 
-func openBrowser(url string) error {
-	var cmd string
-	var args []string
-
-	switch runtime.GOOS {
-	case "linux":
-		cmd = "xdg-open"
-	case "windows":
-		cmd = "rundll32"
-		args = append(args, "url.dll,FileProtocolHandler")
-	case "darwin":
-		cmd = "open"
-	default:
-		return fmt.Errorf("unsupported platform")
-	}
-
-	args = append(args, url)
-	return exec.Command(cmd, args...).Start()
-}
-
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline, oauth2.ApprovalForce)
-	fmt.Printf("Go to the following link in your browser: \n%v\n", authURL)
-
-	err := openBrowser(authURL)
-	if err != nil {
-		log.Fatalf("Unable to open browser: %v", err)
-	}
-
-	// Start a local server to handle the callback
-	codeChan := make(chan string)
-	srv := &http.Server{Addr: "localhost:4040"}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		code := r.URL.Query().Get("code")
-		if code != "" {
-			fmt.Fprintf(w, "Authorization code received. You can close this window.")
-			codeChan <- code
-		} else {
-			fmt.Fprintf(w, "No authorization code received.")
-		}
-	})
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Unable to start server: %v", err)
-		}
-	}()
-
-	code := <-codeChan
-	srv.Shutdown(context.TODO())
-
-	tok, err := config.Exchange(context.TODO(), code)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
-
 func getClient(config *oauth2.Config, userEmail string) *http.Client {
 	tokFile := fmt.Sprintf("token_%s.json", userEmail)
 	tok, err := tokenFromFile(tokFile)
 	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
+		log.Printf("Error reading token file: %v", err)
+		// Instead of getting a new token from the web, we'll use the existing token file
+		tok, err = tokenFromFile("token_m.andres.novrizal@gmail.com.json")
+		if err != nil {
+			log.Fatalf("Unable to read token file: %v", err)
+		}
 	}
 	return config.Client(context.Background(), tok)
 }
@@ -135,16 +80,6 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 	tok := &oauth2.Token{}
 	err = json.NewDecoder(f).Decode(tok)
 	return tok, err
-}
-
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
 }
 
 func CreateGoogleCalendarEvent(senderEmail, summary, description, startDateTime, endDateTime, timeZone string, attendees []string) (*calendar.Event, error) {
@@ -167,7 +102,7 @@ func CreateGoogleCalendarEvent(senderEmail, summary, description, startDateTime,
 		Attendees: make([]*calendar.EventAttendee, 0, len(attendees)),
 	}
 
-	// Hanya tambahkan attendee jika bukan pengirim
+	// Only add attendee if not the sender
 	for _, email := range attendees {
 		if email != senderEmail {
 			event.Attendees = append(event.Attendees, &calendar.EventAttendee{Email: email})
